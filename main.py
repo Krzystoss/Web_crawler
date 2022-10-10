def703fcdf46da11a43c684acdd4d0ee9de7f088
Krzystoss <46158636+Krzystoss@users.noreply.github.com>
2022-10-10 23:11:27 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import argparse
+from link_checker import LinkChecker
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Web crawling')
+    parser.add_argument('--page', type=str, required=True, help='Website link')
+    parser.add_argument('--format', type=str, required=True, choices=['CSV', 'JSON'], help='Select CSV or JSON format')
+    parser.add_argument('--output', type=str, required=True, help='Path to file')
+    args = parser.parse_args()
+
+    scrapper = LinkChecker(args.format, args.output)
+    scrapper.check_link(args.page)
